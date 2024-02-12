f94022e2cf330070c8626a3faed42f98ae3abc2e
NewBornRustacean <huiseomkim@gmail.com>
2024-02-12 10:31:30 +0900

    documents.rs, test_settings.rs  추가

---
+++ src/documents.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::copy;
+
+use reqwest;
+use chrono::{Datelike, Utc, Weekday};
+
+
+
+pub fn download_weekly_fulltext() {
+    let now_utc = Utc::now();
+    let today_utc = now_utc.date_naive();
+    let thursday = today_utc.with_weekday(Weekday::Thu);
+
+    // Format the date as YYMMDD
+    let date_str = format!("{:02}{:02}{:02}", thursday.year() % 100, thursday.month(), thursday.day());
+
+    // Construct the file name and the download URL
+    let file_name = format!("ipa{}.zip", date_str);
+    let download_url = format!("{}{}", BASE_URL, file_name);
+
+    // Send a blocking GET request to the download URL
+    let resp = reqwest::blocking::get(&download_url).expect("request failed");
+
+    // Create the directory to save the file if it does not exist
+    std::fs::create_dir_all(SAVE_DIR).expect("failed to create directory");
+
+    // Create the file in the directory
+    let mut out = File::create(format!("{}/{}", SAVE_DIR, file_name)).expect("failed to create file");
+
+    // Copy the response body to the file
+    copy(&mut resp.bytes().expect("failed to read bytes").as_ref(), &mut out).expect("failed to copy content");
+}
+
+
+pub fn parse_xml(){}
---
+++ tests/test_settings.rs
@@ -0,0 +1,29 @@
+use anyhow::Error;
+use config::{Config, ConfigError};
+use patentpick::settings::Settings;
+
+# [test]
+#[cfg(not(feature = "exclude_from_ci"))]
+fn test_settings_new() {
+    let settings = Settings::new("src/config.toml").unwrap();
+
+    println!("{:?}", settings);
+}
+
+
+// Define another test function to check for errors
+# [test]
+fn test_settings_new_error(){
+    // Use a mock config file path that does not exist
+    let mock_config_file_path = "src/mock_config.toml";
+
+    // Call the Config::builder() method with the mock file path
+    let settings = Config::builder()
+        .add_source(config::File::with_name(mock_config_file_path))
+        .add_source(config::Environment::with_prefix("APP"))
+        .build();
+
+    let error = settings.unwrap_err();
+    println!("{error}");
+
+}
